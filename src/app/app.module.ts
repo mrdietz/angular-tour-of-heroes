60a588c32fabafd85fee560ba97cd417f4e646f4
mrdietz <mrdietz@gmail.com>
2021-03-30 23:54:38 -0700

    End of section 4 - services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
+    MessagesComponent,
 
   ],
   imports: [
